607ac6e7531b492c4f61f578a7aacf19b26d10ba
antleo-com <200936925+c-txt@users.noreply.github.com>
2026-06-10 18:36:39 +0530

    Added Bible

---
+++ bible/_app/immutable/chunks/BOJ3H3gq.js
@@ -0,0 +1,9 @@
+const i=`
+<div class=""><b>CJ-Bible</b></div>
+<div style='line-height:50%'>&nbsp;</div>
+
+<div class="">All text in the public domain</div>
+<div style='line-height:50%'>&nbsp;</div>
+
+<div class="">Public Domain</div>
+`;export{i as default};
---
+++ bible/_app/immutable/entry/start.BVstcXGp.js
@@ -0,0 +1,1 @@
+import{l as o,e as r}from"../chunks/C6Ing3nR.js";export{o as load_css,r as start};
