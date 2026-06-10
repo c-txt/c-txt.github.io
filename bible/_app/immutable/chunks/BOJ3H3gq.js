7f847c8412f0331aad1c0c565954a3462517e2cd
antleo-com <200936925+c-txt@users.noreply.github.com>
2026-06-10 18:42:46 +0530

    deleted bible

--- bible/_app/immutable/chunks/BOJ3H3gq.js
+++
@@ -1,9 +0,0 @@
-const i=`
-<div class=""><b>CJ-Bible</b></div>
-<div style='line-height:50%'>&nbsp;</div>
-
-<div class="">All text in the public domain</div>
-<div style='line-height:50%'>&nbsp;</div>
-
-<div class="">Public Domain</div>
-`;export{i as default};
--- bible/_app/immutable/chunks/DteZYZju.js
+++
@@ -1,1 +0,0 @@
-import{H as t}from"./C6Ing3nR.js";function n(r,o){throw new t(r,o)}export{n as e};
