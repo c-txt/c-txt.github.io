836e535b1bab71c4fa29a291505ed6cf7f2a203e
antleo-com <63607551+antleo-com@users.noreply.github.com>
2026-07-03 07:28:02 +0530

    k

--- cleanop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-find . -name index.md -type f -delete
-find . -path "./.git" -prune -o -path "./pagefind" -prune -o -execdir bash -c 'ghp-nav-cj.sh > index.md' \;
-sed -iE '/Up to Parent folder/d' index.md
-rm index.mdE
